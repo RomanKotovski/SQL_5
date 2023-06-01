85015ef85a2b9273afd3aa0391fc17866eaac5b3
Roman_Kotovski <roman_kotovski@mail.ru>
2023-06-01 18:09:45 +0300

    update

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
+import json
+
 import sqlalchemy
-import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.orm import sessionmaker
+from models import create_tables, Publisher, Book, Shop, Stock, Sale
 
-Base = declarative_base()
-
-class Publisher(Base):
-    __tablename__ = "publisher"
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=40), unique=True)
-
-class Book(Base):
-    __tablename__ = "book"
-    id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(length=80), unique=True)
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-
-    publisher = relationship(Publisher, backref="book")
-
-def create_tables(engine):
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
 
 DSN = "postgresql://postgres:956841@localhost:5432/books_db"
 engine = sqlalchemy.create_engine(DSN)
@@ -30,6 +13,27 @@ create_tables(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+with open('fixtures/tests_data.json', 'r') as fd:
+    data = json.load(fd)
+
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+session.commit()
+
+# for c in session.query(Publisher):
+#     print(c)
+
+writer = input(f'Введите имя издателя: ')
+selected = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale).filter(Publisher.name.like(writer))
+for s in selected.all():
+    print(f'{s[0]} | {s[1]} | {str(s[2])} | {str(s[3])}')
 
 
 session.close()
